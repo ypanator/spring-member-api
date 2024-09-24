1bc7ba1c53abf759ee70a784306791395aa8d15d
asPounder <karlossokolowski75@gmail.com>
2024-09-24 16:38:22 +0200

    Initial commit

---
+++ src/main/java/api/members/members_api/MembersApiApplication.java
@@ -0,0 +1,13 @@
+package api.members.members_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MembersApiApplication.class, args);
+	}
+
+}

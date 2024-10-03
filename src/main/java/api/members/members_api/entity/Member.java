7c98ae25c127a77fe49d0eb4757c206a30e328b1
ypanator <153828092+ypanator@users.noreply.github.com>
2024-10-03 07:58:37 +0000

    Change `@NotEmpty` to `@NotBlank`

--- src/main/java/api/members/members_api/entity/Member.java
+++ src/main/java/api/members/members_api/entity/Member.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,8 +17,8 @@ public class Member {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @NotEmpty
+    @NotBlank
     private String firstName;
-    @NotEmpty
+    @NotBlank
     private String lastName;
 }

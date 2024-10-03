7ff4b6cb09af72c4e963ce6bc60a98c4109a56d7
ypanator <153828092+ypanator@users.noreply.github.com>
2024-10-03 07:56:41 +0000

    Implement AuthRequest.java

--- src/main/java/api/members/members_api/auth/AuthRequest.java
+++ src/main/java/api/members/members_api/auth/AuthRequest.java
@@ -1,5 +1,13 @@
 package api.members.members_api.auth;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class AuthRequest {
     
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String password;    
 }

9f666437be0731d9f8f3c38281ed13461abc5eb4
ypanator <153828092+ypanator@users.noreply.github.com>
2024-10-04 09:07:44 +0000

    Update User.java

---
+++ src/main/java/api/members/members_api/entity/Role.java
@@ -0,0 +1,7 @@
+package api.members.members_api.entity;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_MANAGER,
+    ROLE_ADMIN
+}
--- src/main/java/api/members/members_api/entity/User.java
+++ src/main/java/api/members/members_api/entity/User.java
@@ -1,8 +1,20 @@
 package api.members.members_api.entity;
 
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -10,7 +22,7 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity @Data
-public class User {
+public class User implements UserDetails {
     
     @Id @JsonIgnore
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,6 +30,18 @@ public class User {
 
     @NotBlank
     private String username;
+
     @NotBlank
     private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+            .map(role -> new SimpleGrantedAuthority(role.name()))
+            .collect(Collectors.toSet());
+    }
 }

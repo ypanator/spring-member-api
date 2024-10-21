2b99c51b50e910a6dae6ab5a40917e8df76abc72
ypanator <153828092+ypanator@users.noreply.github.com>
2024-10-21 10:48:51 +0000

    Add `@NonNull` to JwtFilter.java

--- src/main/java/api/members/members_api/auth/JwtFilter.java
+++ src/main/java/api/members/members_api/auth/JwtFilter.java
@@ -2,9 +2,9 @@ package api.members.members_api.auth;
 
 import java.io.IOException;
 
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -23,7 +23,8 @@ public class JwtFilter extends OncePerRequestFilter {
     final UserService userService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(
+        @NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
             throws ServletException, IOException {
         final String header = request.getHeader("Authorization");
 
